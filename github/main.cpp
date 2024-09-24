ffeeef06a63857b73a960877595cf280a42bfc31
Mihir Arvindkumar Patel <Mpatel5872@conestogac.on.ca>
2024-09-23 23:48:03 -0400

    changing funtion

--- github/main.cpp
+++ github/main.cpp
@@ -9,13 +9,19 @@ struct STUDENT_DATA {
     std::string lastName;
 };
 
-int main() {
-    std::vector<STUDENT_DATA> students;
-    std::ifstream inputFile("StudentData.txt");
+struct StudentInfo {
+    std::string first_name;
+    std::string last_name;
+    std::string email;
+};
+
+// Function to read data from "StudentData.txt"
+void readStudentData(const std::string& filename, std::vector<STUDENT_DATA>& students) {
+    std::ifstream inputFile(filename);
 
     if (!inputFile.is_open()) {
-        std::cerr << "Could not open the file!" << std::endl;
-        return 1;
+        std::cerr << "Could not open the file: " << filename << std::endl;
+        return;
     }
 
     std::string line;
@@ -32,13 +38,52 @@ int main() {
     }
 
     inputFile.close();
+}
 
+// Function to read data from "StudentData_Emails.txt"
+void readStudentDataEmails(const std::string& filename, std::vector<StudentInfo>& students2) {
+    std::ifstream infile(filename);
+    if (!infile.is_open()) {
+        std::cerr << "Error opening file: " << filename << std::endl;
+        return;
+    }
+
+    std::string line;
+    while (std::getline(infile, line)) {
+        std::stringstream ss(line);
+        StudentInfo student2;
+
+        // Parsing the line (format: last_name, first_name, email)
+        std::getline(ss, student2.last_name, ',');
+        std::getline(ss, student2.first_name, ',');
+        std::getline(ss, student2.email, ',');
+
+        // Add student to the vector
+        students2.push_back(student2);
+    }
+
+    infile.close();
+}
+
+// Function to print student data from "StudentData_Emails.txt"
+void printStudents(const std::vector<StudentInfo>& students2) {
+    for (const auto& student : students2) {
+        std::cout << student.first_name << " " << student.last_name << " - " << student.email << std::endl;
+    }
+}
+
+int main() {
+    std::vector<STUDENT_DATA> students;
+
+    // Call the function to read data from "StudentData.txt"
+    readStudentData("StudentData.txt", students);
+
+    // Check if the code is running in Pre-Release or Standard mode
 #ifdef PRE_RELEASE
     std::cout << "Running Pre-Release Version" << std::endl;
 #else
     std::cout << "Running Standard Version" << std::endl;
 #endif
-
 
     // Print all student information in debug mode
 #ifdef _DEBUG
@@ -47,13 +92,12 @@ int main() {
     }
 #endif
 
+    // Pre-release code to handle reading emails
 #ifdef PRE_RELEASE
-    std::ifstream emailFile("StudentData_Emails.txt");
-    // Additional logic for handling emails
-    emailFile.close();
+    std::vector<StudentInfo> students2;
+    readStudentDataEmails("StudentData_Emails.txt", students2);
+    printStudents(students2);
 #endif
- 
-    
 
     return 0;
 }
